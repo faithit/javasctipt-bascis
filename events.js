e4112ff9182f9c911fb986126365fae86099a734
faithit <faithittrainer@gmail.com>
2023-07-05 09:37:14 +0300

    more

---
+++ events.js
@@ -0,0 +1,10 @@
+function btnclick(){
+    alert("hello you clicked a button");
+}
+ function bdload(){
+    alert("the body has loaded")
+ }
+ //addeventlistener
+ //elememt.addeventlistener(event,function)
+document.querySelector(.btn)
+
