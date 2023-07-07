704b301a7ece6c658c0c55dbd1cc6ad15c051503
faithit <faithittrainer@gmail.com>
2023-07-07 09:12:23 +0300

    more

---
+++ objects.js
@@ -0,0 +1,9 @@
+//declaration of an object
+const person = { //creating an object person
+    firstname:"john", //key firstname with value john
+    lastname:"doe",
+    age:20,
+    height:6.2,
+    weight:50
+}
+//accessing the property
